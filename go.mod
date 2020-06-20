4cc2577b184348997f1f122d19dccc440dc5e645
Md. Arafat Azam <arafatazam17@gmail.com>
2020-06-21 01:55:17 +0600

    db connection and dotenv

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/arafatazam/go-crud-demo
 
 go 1.14
 
-require github.com/gofiber/fiber v1.11.1
+require (
+	github.com/gofiber/fiber v1.11.1
+	github.com/joho/godotenv v1.3.0
+	github.com/jmoiron/sqlx v1.2.0
+	github.com/lib/pq v1.0.0
+)
--- main.go
+++ main.go
@@ -1,13 +1,35 @@
 package main
 
-import "github.com/gofiber/fiber"
+import (
+  "log"
+  "os"
+
+  "github.com/joho/godotenv"
+  _ "github.com/lib/pq"
+  "github.com/jmoiron/sqlx"
+  "github.com/gofiber/fiber"
+)
 
 func main() {
+  err := godotenv.Load()
+  if err != nil {
+    log.Fatal("Error loading .env file")
+  }
+
+  dbUrl := os.Getenv("DB_URL")
+
+  db := sqlx.MustConnect("postgres", dbUrl)
+
+  err = db.Ping()
+  if err != nil {
+	  log.Panic("Database Connection Failure")
+  }
+
   app := fiber.New()
 
-  app.Get("/", func (c *fiber.Ctx) {
-    c.Send("Hello, World!")
+  app.Post("/users", func (c *fiber.Ctx) {
+    c.Send("Create User")
   })
-
-  app.Listen(3000)
+  
+  app.Listen(os.Getenv("PORT"))
 }
