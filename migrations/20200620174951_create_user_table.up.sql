2606028a177f260b7de0e1eba6a96e1734acd41f
Md. Arafat Azam <arafatazam17@gmail.com>
2020-06-22 03:42:49 +0600

    Fix: migration character to varchar

--- migrations/20200620174951_create_user_table.up.sql
+++ migrations/20200620174951_create_user_table.up.sql
@@ -1,11 +1,11 @@
 BEGIN;
 CREATE TABLE "users" (
-    "id" character(40) NOT NULL,
-    "email" character(100) NOT NULL,
-    "first_name" character(50) NOT NULL,
-    "last_name" character(50),
-    "address" character(50) NOT NULL,
-    "phone" character(20),
+    "id" varchar(40) NOT NULL,
+    "email" varchar(100) NOT NULL,
+    "first_name" varchar(50) NOT NULL,
+    "last_name" varchar(50),
+    "address" varchar(50) NOT NULL,
+    "phone" varchar(20),
     CONSTRAINT "users_email" UNIQUE ("email"),
     CONSTRAINT "users_id" PRIMARY KEY ("id")
 ) WITH (oids = false);
